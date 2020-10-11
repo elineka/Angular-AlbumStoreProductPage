020c3ec9c9d0890a7ec58206bc2666363ad8b2f9
unknown <you@example.com>
2020-10-12 00:52:29 +0300

    Displaying a List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   
+  private _productsUrl: string = '../assets/products.json';
   
 
   constructor(private _http: Http) { }
@@ -15,6 +17,9 @@ private _albumUrl: string = '../assets/album.json'
  getAlbum(id: number): Observable<Album> {
  return this._http.get(this._albumUrl).map(response => <Album>response.json());
 
+}
+getProducts(): Observable<Product[]> {
+  return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
 }
 
 }
